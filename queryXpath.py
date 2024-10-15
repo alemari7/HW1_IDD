46911949b07d12272babb898387c0468d4a0380c
Riccardo Felici <riccardofelici7@gmail.com>
2024-10-15 16:26:39 +0200

    footnotes XPath query
    aggiornata la query XPath per calcolare le footnote

--- queryXpath.py
+++ queryXpath.py
@@ -1,6 +1,21 @@
 import os
 import json
 from lxml import etree
+from bs4 import BeautifulSoup
+import html
+
+def html_to_text(html_string):
+    # Usa BeautifulSoup per eliminare i tag e ottenere il testo
+    soup = BeautifulSoup(html_string, 'html.parser')
+    
+    # Decodifica le entità HTML come &#8220; o &#8209;
+    decoded_text = html.unescape(soup.get_text())
+
+    return decoded_text
+
+def html_array_to_text(html_array):
+    # Applica html_to_text a ogni stringa nell'array
+    return [html_to_text(html_string) for html_string in html_array]
 
 # Funzione per eseguire una query XPath su un file XML o HTML
 def execute_xpath_query(file_path, xpath_query):
@@ -60,21 +75,33 @@ def process_all_html_files():
                 
                 query_caption = f'//table[@id="{table_id}"]/ancestor::figure//figcaption/text()'
                 query_table = f'//table[@id="{table_id}"]/ancestor::figure//table'
-                query_footnotes = f'//table[@id="{table_id}"]/ancestor::figure//sup/text()'
+                query_footnotes_id = f'//table[@id="{table_id}"]/ancestor::figure//cite/a/@href'
                 
                 if(figure_id):
                     query_references = f"//*[substring(@href, string-length(@href) - string-length('#{figure_id[0]}') + 1) = '#{figure_id[0]}']/ancestor::p" 
                 else:
                     query_references = f"//*[substring(@href, string-length(@href) - string-length('#{figure_id}') + 1) = '#{figure_id}']/ancestor::p" 
 
+                footnote_ids = execute_xpath_query(file_path, query_footnotes_id) or []
+                
+                footnotes_results = []
+                for footnote_id in footnote_ids: 
+                    footnote_id = footnote_id.replace("#", "")    
+                    query_footnotes = f'//li[@id="{footnote_id}"]/*/text()'
+                    footnotes_results.append(execute_xpath_query(file_path, query_footnotes) or [])
+
+#                if footnotes_results != []:
+#                    footnotes_results = html_array_to_text(footnotes_results)
+#                print("footnotes ", footnotes_results)
+                
+
                 caption_results = execute_xpath_query(file_path, query_caption) or []
                 table_results = execute_xpath_query(file_path, query_table) or []
-                footnote_results = execute_xpath_query(file_path, query_footnotes) or []
                 reference_results_raw = execute_xpath_query(file_path, query_references) or []
 
                 reference_results = []
                 for elem in reference_results_raw:
-                    reference_results.append(etree.tostring(elem, pretty_print=True).decode("utf-8"))
+                    reference_results.append(html_to_text(etree.tostring(elem, pretty_print=True).decode("utf-8")))
 
                 if table_results:
                     table_html = ''.join([etree.tostring(result, pretty_print=True).decode() for result in table_results])
@@ -84,7 +111,7 @@ def process_all_html_files():
                 file_results[table_id] = {
                     "caption": ' '.join(caption_results) if caption_results else None,
                     "table": table_html,
-                    "footnotes": footnote_results,
+                    "footnotes": footnotes_results,
                     "references": reference_results
                 }
 
